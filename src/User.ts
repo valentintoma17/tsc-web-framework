a5a433e128afdd1d3e3b31a68946074104e5b3ff
Valentin Toma <valentin.toma17@gmail.com>
2023-10-11 20:32:24 +0300

    Create User + db

---
+++ src/User.ts
@@ -0,0 +1,38 @@
+interface IUserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: IUserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  set(update: IUserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string) {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Heya world!');
+import { User } from './User';
+
+const data = new User({ name: 'Name', age: 14 });
+
+data.on('change', () => {
+  console.log('I have spoken');
+});
+
+data.trigger('change');
